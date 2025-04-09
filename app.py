dbffe403635a9f56932d7a5d0de664ed84e68bda
yugli <dev.maksim.sharay@gmail.com>
2025-04-10 00:06:50 +0100

    Add route for home page to improve navigation

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html', title='Home')
 
