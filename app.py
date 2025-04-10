c7291d7254e40458d928754e19fa582c955cf066
yugli <dev.maksim.sharay@gmail.com>
2025-04-10 01:52:56 +0100

    Add about page template with product details and features for Dovery app

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
